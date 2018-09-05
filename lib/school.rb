2f7f76a549eeeeec0849ceab382eb2f8331c9597
Stacy Carr <stacy@boostersinabox.com>
2018-09-05 15:51:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = { }
+  end
+  
+  def add_student(student_name, grade)
+    roster[grade] = ||= []
+    roster[grade] << student_name
+  end
+    
+    
+  
+  
+  
+  
+  
+  
+end
